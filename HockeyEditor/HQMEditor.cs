8e1a1f153743d6446564c010141bf40133902cc7
Callum Neideck <cal_neideck@hotmail.com>
2016-03-07 20:53:46 +1100

    Added operators for HQMVector

--- HockeyEditor/HQMEditor.cs
+++ HockeyEditor/HQMEditor.cs
@@ -91,5 +91,35 @@ namespace HockeyEditor
                 return new HQMVector(X / m, Y / m, Z / m);
             }
         }
+
+        public HQMVector operator +(HQMVector left, HQMVector right)
+        {
+            return new HQMVector(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+        }
+
+        public HQMVector operator -(HQMVector left, HQMVector right)
+        {
+            return new HQMVector(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+        }
+
+        public HQMVector operator *(HQMVector vector, float scale)
+        {
+            return new HQMVector(vector.X * scale, vector.Y * scale, vector.Z * scale);
+        }
+
+        public HQMVector operator /(HQMVector vector, float scale)
+        {
+            return new HQMVector(vector.X / scale, vector.Y / scale, vector.Z / scale);
+        }
+
+        public bool operator ==(HQMVector left, HQMVector right)
+        {
+            return left.X == right.X && left.Y == right.Y && left.Z == right.Z;
+        }
+
+        public bool operator !=(HQMVector left, HQMVector right)
+        {
+            return !(left == right);
+        }
     }
 }
